648841fb2bc06a9f7c3d4576e1591ca305e52ff5
AzerArfa <120283616+AzerArfa@users.noreply.github.com>
2024-04-25 16:26:59 +0200

    Delete Offre directory

--- Offre/src/main/java/com/gao/Offre/OffreRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gao.Offre;
-
-import java.util.UUID;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OffreRepository extends JpaRepository<Offre,UUID> {
-}
